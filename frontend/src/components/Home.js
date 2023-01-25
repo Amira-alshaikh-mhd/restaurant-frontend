a6096376af6bd280f20f2ee7066ca89bd4e73a90
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2023-01-25 11:40:53 +0200

    1 step

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import logo from './logo.jpg';
+
+
+function Home(){
+    
+      return (
+        <>
+        <img src={logo} alt='logo for rest'></img>
+  
+      <h1>Restaurant</h1>
+      </>
+  
+      );
+    }
+  
+  
+  
+
+export default Home;
