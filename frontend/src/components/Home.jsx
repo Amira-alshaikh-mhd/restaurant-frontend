e115764524e8e21c376c4f3d1439bdbeebc60bc6
amira-alshaikh-mhd <amira.alshaikh.mhd@gmail.com>
2023-02-06 14:44:34 +0200

    get

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,12 +11,11 @@ const Categories = (props) => {
     const [categories, setCategories] = useState([]);
     const [items, setItems] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState({});
-    // const [nana, setNana] = useState('63cfad2d07dffe46b40cf2c4')
-    // const [dodo, setDodo] = useState('63d978aa0e4f648cbb478e20')
+    
 
     const fetchCategories = async (resto_Id) => {
         try {
-            const response = await axios.get(`http://localhost:3000/categories?resto_id=63cfad2d07dffe46b40cf2c4`);
+            const response = await axios.get(`http://localhost:3000/get?resto_id=63cfad2d07dffe46b40cf2c4`);
             setCategories(response.data);
         } catch (error) {
             console.error(error);
@@ -26,7 +25,7 @@ const Categories = (props) => {
     const fetchItems = async (cat_Id) => {
         console.log("zeinab ",cat_Id)
         try {
-            let response = await axios.get(`http://localhost:3000/categories/items?cat_id=63d978aa0e4f648cbb478e20`);
+            let response = await axios.get(`http://localhost:3000/get/items?cat_id=63d978aa0e4f648cbb478e20`);
             // http://localhost:3000/categories/items?cat_id=63d978aa0e4f648cbb478e20
             console.log("response ",response)
             setItems(response.data);
@@ -54,57 +53,7 @@ const Categories = (props) => {
 
 
 
-    // function Home() {
-    //     const[data, setData] = useState();
-    //     const [am,setam]=useState();
-    //     const [meme,setmeme]=useState();
-
-    //     const [nana,setnana]=useState('63d27dca5e79a7c3ea468aaa')
-    // const dodo={resto_id:nana}
-    //     const getthecat_id=(_id)=>{
-    //         console.log(_id)
-    //     }
-
-
-    // const amira = async()=>{
-    //     const dol=`http://localhost:3000/categories?resto_id=63cfad2d07dffe46b40cf2c4`
-
-    //     const { data }=await axios.get(dol);
-
-    //     setam(data)
-
-
-    // }
-    // const getitems=async()=>{
-    //     const uritems=`http://localhost:3000/categories/items?cat_id=63d978aa0e4f648cbb478e20`
-    //     const {itemsdada}=await axios.get(uritems,)
-    // }
-
-    // useEffect(() => {
-    //     amira()
-    //   },[]);
-
-
-
-
-
-
-
-
-
-    //   const [mero,setmero]=useState('63d978aa0e4f648cbb478e20')
-    //   const amora={cat_id:mero}
-    //   const [itemName, setitemName] =useState('')
-    //   const [itemDesc, setitemDesc] =useState('')
-    //   const [itemPrice, setitemPrice] =useState('')
-    //   const [itemTag, setitemTag] =useState([''])
-
-    //   useEffect(() => {
-    //     axios.get("http://localhost:3000/categories/items?cat_id=63d978aa0e4f648cbb478e20")
-    //     .then((res) =>{
-    //         setData(res, data);
-    //     });
-    //   }, [meme]);
+  
 
 
 
@@ -184,33 +133,3 @@ export default Categories;
 
 
 
-{/* <nav>
-                {am.map(({ resto_id, category_name }) => (
-
-
-                    <div key={resto_id} className='wrapper'>
-                        <button type='submit'  >{category_name}</button>
-
-
-                    </div>
-                )
-                )}
-            </nav> */}
-
-
-
-
-{/* 
-// am?.map((hourframe,index)=>(
-    //     <div key={hourframe._id} className='wrapper'>
-    //                     <button onClick={console.log(hourframe._id)}>{hourframe.category_name} {hourframe._id}</button>
-
-
-    //                 </div>  
-    // )) */}
-
-
-
-{/* { id, item_name, item_price, item_description, item_tags } */ }
-
-      // {meme?.map((itemss, index) => (
