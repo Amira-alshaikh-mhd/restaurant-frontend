9478db5a120d5411fe7eef3250f4030765ba2295
ALI HOMSI <truphleg@gmail.com>
2023-02-07 16:31:34 +0200

    message

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -12,21 +12,21 @@ function admin(){
         <>
         {/*RESTORANT LOGO*/}
         <div className='col-1 container'>
-        <img src={logo} alt='logo for rest'></img>
-        <label id='imgbtn' htmlFor="filePicker" >
-<img id='fileimg' src={edit} alt=''></img>
+        <img className='logo' src={logo} alt='logo for rest'></img>
+        <label className='imgbtn' htmlFor="filePicker" >
+<img className='fileimg' src={edit} alt=''></img>
        </label>
-<input id="filePicker" type={"file"}></input>
+<input className="filePicker" type={"file"}></input>
 
         </div>
            {/*RESTORANT NAME */}
          <div className='col-2 container2'>
             <h1 className='h11'>Restaurant </h1>
-            <button id='resname' type='button'><img id='editname' src={edit} alt=''></img></button>
+            <button className='resname' type='button'><img className='editname' src={edit} alt=''></img></button>
             
             </div>
             {/*NAV BAR*/}
-            <nav>
+            <nav className='nav'>
             <div className='col-3 wrapper'>
                 <div className='navbtn'></div>
              <div className='twobtn'>
@@ -96,11 +96,11 @@ function admin(){
        {/*DISH*/}
       <div className='col-4 dish1'>
       <div className='editbtn1'>
-        <h3>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   </h3>
-        <button id='edit1' type='button'><img id='editname' src={edit} alt=''></img></button>
-        <button id='edit1' type='button'><img id='editname' src={bin} alt=''></img></button>
+         <h3 className='h3'>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   </h3>
+        <button className='edit1' type='button'><img className='editname' src={edit} alt=''></img></button>
+        <button className='edit1' type='button'><img className='editname' src={bin} alt=''></img></button>
         </div>
-        <b>price</b>
+        <b className='price'>price</b>
         <p>description description description description description description description description description description description description description</p>
         <p>spicy</p>
     </div>
@@ -109,11 +109,11 @@ function admin(){
     <div className='col-4 dish1'>
         <div className='editbtn1'>
    
-        <h3>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </h3>
-        <button id='edit1' type='button'><img id='editname' src={edit} alt=''></img></button>
-        <button id='edit1' type='button'><img id='editname' src={bin} alt=''></img></button>
+         <h3 className='h3'>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </h3>
+        <button className='edit1' type='button'><img className='editname' src={edit} alt=''></img></button>
+        <button className='edit1' type='button'><img className='editname' src={bin} alt=''></img></button>
         </div>
-        <b>price</b>
+        <b className='price'>price</b>
         <p>description</p>
         <p>spicy</p>
     </div>
@@ -121,11 +121,11 @@ function admin(){
 
     <div className='col-4 dish1'>
     <div className='editbtn1'>
-        <h3>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </h3>
-        <button id='edit1' type='button'><img id='editname' src={edit} alt=''></img></button>
-        <button id='edit1' type='button'><img id='editname' src={bin} alt=''></img></button>
+         <h3 className='h3'>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </h3>
+        <button className='edit1' type='button'><img className='editname' src={edit} alt=''></img></button>
+        <button className='edit1' type='button'><img className='editname' src={bin} alt=''></img></button>
         </div>
-        <b>price</b>
+        <b className='price'>price</b>
         <p>description</p>
         <p>spicy</p>
     </div>
@@ -133,11 +133,11 @@ function admin(){
 
     <div className='col-4 dish1' >
     <div className='editbtn1'>
-        <h3>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</h3>
-        <button id='edit1' type='button'><img id='editname' src={edit} alt=''></img></button>
-        <button id='edit1' type='button'><img id='editname' src={bin} alt=''></img></button>
+         <h3 className='h3'>dish &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</h3>
+        <button className='edit1' type='button'><img className='editname' src={edit} alt=''></img></button>
+        <button className='edit1' type='button'><img className='editname' src={bin} alt=''></img></button>
         </div>
-        <b>price</b>
+        <b className='price'>price</b>
         <p>description</p>
         <p>spicy</p>
     </div>
