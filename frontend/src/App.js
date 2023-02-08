f8f4f5c94b63560440a274f486f4ac2bff31db19
shant-k <shantk650@gmail.com>
2023-02-08 19:51:44 +0200

    added routes to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 
 import React from 'react';
 import './App.css';
-import Signin from './components/signin';
+import Signin from './Pages/signin';
+import Signup from './Pages/signup';
+import{ BrowserRouter, Route, Routes }from "react-router-dom";
+
 
 function App() {
+  return(
+  <>
   
-    return (
-
-    <Signin />
-
-    );
+  <BrowserRouter>
+<Routes>
+<Route path='/users/login' exact element={<Signin/>} />
+<Route path='/users/new' exact element={<Signup/>} />
+</Routes>
+</BrowserRouter>
+    </>
+  );
+   
   }
 
 
